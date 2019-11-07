520e7df6acb2f76d6efd9717b415ba4078e76c1e
git-konick <avtoorbita@gmai.com>
2019-11-07 20:41:19 +0300

    .

---
+++ guessinggame.sh
@@ -0,0 +1,23 @@
+ #!/usr/bin/env bash
+ # File: guessinggame.sh
+
+ echo "Type in a string and then press Enter:"
+  read response
+   echo "You entered: $response"
+if [[ $response -eq $(ls | wc -l) ]] # determine the exact value
+ then
+  for code in $(ls)  # determine the final output
+   do
+    echo "$code [contents of current directory]"
+   done
+    echo ""
+    echo "Congratulations! $response is a great number!!!"
+    echo "End programm."
+ elif [[ $response -gt $(ls | wc -l) ]] # determined by the flag -gt
+   then
+    echo "$response > (x) >>>>> (X) value is less."
+      bash guessinggame.sh
+ else
+    echo "$response < (x) <<<<< (X) value is greater."
+      bash guessinggame.sh
+fi
