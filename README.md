a6f7005bcd93b9256d0ef1f03a760786dbdd43be
git-konick <avtoorbita@gmai.com>
2019-11-13 15:08:41 +0300

    recreated

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # **Project Unix-Workbench(KG)** Bash, Make, Git and Github
 
 The date and time at which make was run is:
-Сб. нояб.  9 13:27:58 MSK 2019
+Ср. нояб. 13 14:56:22 MSK 2019
 
 The number of lines of code contained in guessinggame.sh is:
-23
+36
--- guessinggame.sh
+++ guessinggame.sh
@@ -1,23 +1,36 @@
- #!/usr/bin/env bash
- # File: guessinggame.sh
+#!/usr/bin/env bash
+# File: guessinggame.sh
 
- echo "How may files are in the current directory? Please, Enter number:"
-  read response
-   echo "You entered : $response"
-if [[ $response -eq $(ls | wc -l) ]] # determine the exact value
- then
-  for code in $(ls)  # determine the final output
-   do
-    echo "$code [contents of current directory]"
-   done
-    echo ""
-    echo "Congratulations! $response is a great number!!!"
-    echo "End programm."
- elif [[ $response -gt $(ls | wc -l) ]] # determined by the flag -gt
-   then
-    echo "$response > (x) >>>>> (X) value is less."
-      bash guessinggame.sh
- else
-    echo "$response < (x) <<<<< (X) value is greater."
-      bash guessinggame.sh
-fi
+# Get the number of files in the current directory
+num_files=$(ls -1 | wc -l)
+
+# to evaluate a user guess
+function guess {
+
+	if ! [[ $slv =~ ^[0-9]+$ ]]
+        then
+        echo "wrong entry, enter a number"
+
+	elif [[ $num_files -eq $slv ]]
+	then
+	echo "Congratulations!"
+
+	elif [[ $num_files -gt $slv ]] 
+	then
+	echo "Your guess is too low."
+
+	elif [[ $num_files -lt $slv ]]
+	then
+	echo "Your guess is too high."
+
+	fi
+
+}
+ # Loop - breaks if = 0 
+	while [[ $num_files -ne $slv ]]
+do
+
+	read -p "How many files are in the current directory: " slv
+	echo $(guess $num_files $slv)
+
+done
